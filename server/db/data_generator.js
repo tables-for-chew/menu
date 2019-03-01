97381182d1353a4de7d3376ee1fbf9324b0f048e
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-02-28 22:03:02 -0800

    Implement write stream with buffer

--- server/db/data_generator.js
+++ server/db/data_generator.js
@@ -17,70 +17,60 @@ const foodItem = (id, restaurant_id) => {
   foodItem.restaurant_id = restaurant_id; 
   return foodItem;
 }
-
 console.log(JSON.stringify(foodItem(2,37)));
 
-// const generateMenus = () => {
-//   const foodId = 0;
-//   const meals = '';
-  
+//stack the buffer with 10,000 items
+//write them to the file and when they are finished
 
-//   //generate one menu
-//   const oneMenu = () => {
-//     const oneMenu = '';
-//     for (let i = 0; i < 100; i++) {
-//       let foodId = 
-//       foodItem(foodId, i)
-//       foodId += 1; 
-//     }
-//   } 
+//set before running this file
+const bufferItems = 10;
+const totalRecords = 30;
 
-//   var menuStream = fs.createWriteStream('./menu_data/lunches.csv');
-//   menuStream.write(...,'UTF8');
-// }
+//counts
+var fileWriteCount = 0; 
+var itemCount = 1; 
 
-const writeStream = () => {
-  
-  var createWriteStream = Promise.promisify(fs.createWriteStream);
-  
-  createWriteStream('./menu_data/output.json', "utf8").then(() => {
-    var data = JSON.stringify(foodItem(2,37));
-    console.log(data);
-
-    // Write the data to stream with encoding to be utf8
-    writerStream.write(data,'UTF8');
-    // Mark the end of file
-    writerStream.end();
-    // Handle stream events --> finish, and error
-    writerStream.on('finish', function() {
-      console.log("Write completed.");
-    });
-    writerStream.on('error', function(err) {
-      console.log(err.stack);
-    });
-    
-  }).then(() => {
-    console.log("Program Ended");
-    
-  }).catch(SyntaxError, function(e) {
-      console.log("File had syntax error", e);
-
-  // // Create a writable stream
-  // var writerStream = fs.createWriteStream('./menu_data/output.json');
-  // // Write the data to stream with encoding to be utf8
-  // writerStream.write(data,'UTF8');
-  // // Mark the end of file
-  // writerStream.end();
-  // // Handle stream events --> finish, and error
-  // writerStream.on('finish', function() {
-  //    console.log("Write completed.");
-  // });
-  // writerStream.on('error', function(err) {
-  //    console.log(err.stack);
-  // });
-  // console.log("Program Ended");
+//returns an array of items => [{"id":2,"name":"rem","description":"Eum ab laudantium saepe et.","price":74,"restaurant_id":37}]
+const loadBuffer = () => {
+  var buffer = [];
+  for (let i = 0; i < bufferItems; i++) {
+    const randomRestaurantId = randomNumber(totalRecords)
+    var newItem = foodItem(itemCount, randomRestaurantId);
+    buffer.push(newItem);
+    itemCount++;
+    console.log('item', itemCount);
+  }
+  return buffer; 
 }
 
+//writes a loadBuffer batch to a file...
+const writeStream = () => {  
+
+    //stringify here...
+    var data = loadBuffer();
+    var stringifiedData = JSON.stringify(data);
+
+    // Create a writable stream
+    var writerStream = fs.createWriteStream('./menu_data/output.json');
+
+    // Write the data to stream with encoding to be utf8
+    writerStream.write(stringifiedData,'UTF8');
+
+    // Mark the end of file
+    writerStream.end();
+
+    // Handle stream events --> finish, and error
+    writerStream.on('finish', function() {
+       console.log("Write completed.");
+    });
+    writerStream.on('error', function(err) {
+       console.log(err.stack);
+    });
+    console.log("Program Ended");
+  }
+
+  writeStream();
+  
 
 
 /*
