9c65ee4c74278abe1142ecb3e569b35e11869315
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-03-11 12:31:31 -0700

    adds newrelic and artillery config to repo

---
+++ newrelic.js
@@ -0,0 +1,24 @@
+'use strict'
+
+exports.config = {
+  app_name: ['Menus'],
+  license_key: 'f148cf775f9971c0767fa930779d3b91497fb307',
+  logging: {
+    level: 'info'
+  },
+  allow_all_headers: true,
+  attributes: {
+    exclude: [
+      'request.headers.cookie',
+      'request.headers.authorization',
+      'request.headers.proxyAuthorization',
+      'request.headers.setCookie*',
+      'request.headers.x*',
+      'response.headers.cookie',
+      'response.headers.authorization',
+      'response.headers.proxyAuthorization',
+      'response.headers.setCookie*',
+      'response.headers.x*'
+    ]
+  }
+}
