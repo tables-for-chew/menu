e7fd81f44b100daf1ccbdbc4b287e153b7bcfca7
Acacia Pappas <acacia.m.pappas@gmail.com>
2019-03-12 19:43:11 -0700

    refactor sql postgres load files, update example config

--- server/db/utils/postgresql/loadDessertPsql.sql
+++ server/db/utils/postgresql/loadDessertPsql.sql
@@ -1,7 +1,6 @@
 \COPY dessert(id,name,description,restaurant_id) FROM '/Users/acaciapappas/Desktop/TableForChew/menu/server/db/utils/generator/menu_data/dessert.csv' DELIMITER ',' CSV HEADER;
 
 -- SELECT setval('dessert_id_seq', (SELECT MAX(id) FROM dessert)+1);
-ALTER TABLE "dinner_id_seq" RESTART WITH 200000002;
-
+ALTER SEQUENCE "dinner_id_seq" RESTART WITH ...;
 
 CREATE INDEX restaurant_id_index_dessert ON dessert(restaurant_id);
--- server/db/utils/postgresql/loadLunchPsql.sql
+++ server/db/utils/postgresql/loadLunchPsql.sql
@@ -1,6 +1,6 @@
 \COPY lunch(id,name,description,restaurant_id) FROM '/Users/acaciapappas/Desktop/TableForChew/menu/server/db/utils/generator/menu_data/lunch.csv' DELIMITER ',' CSV HEADER;
 
 -- SELECT setval('lunch', (SELECT MAX(id) FROM lunch)+1);
-ALTER TABLE "lunch_id_seq" RESTART WITH 200000002;
+ALTER SEQUENCE "lunch_id_seq" RESTART WITH 200000002;
 
 CREATE INDEX restaurant_id_index_lunch ON lunch(restaurant_id);
